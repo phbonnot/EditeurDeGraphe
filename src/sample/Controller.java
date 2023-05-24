6c0f98b5bcfb31fbb031942d46553fb451edf5cf
philippebonnot <phbonnot@gmail.com>
2023-05-24 13:49:50 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class Controller {
+
+    @FXML
+    void detectionSouris(MouseEvent event){
+
+    }
+
+    @FXML
+    void creationSommet(ActionEvent event) {
+
+    }
+
+    @FXML
+    void definirLibellesChiffres(ActionEvent event) {
+
+    }
+
+    @FXML
+    void definirLibellesLettres(ActionEvent event) {
+
+    }
+
+    @FXML
+    void creationArete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selection(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+
+}
